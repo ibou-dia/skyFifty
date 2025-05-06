621a4e2fe25feb75febba3469263c04614fe8738
ibou-dia <ibrahimadia800@gmail.com>
2025-05-06 00:21:37 +0000

    test resolve

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   // Configuré pour GitHub Pages - le nom du dépôt GitHub
-  basePath: process.env.NODE_ENV === 'production' ? '/public' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/test-portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/public/' : '',
   images: {
     unoptimized: true, // Désactive l'optimisation des images pour l'export statique
